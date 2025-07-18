6186fdca399126dc607320f1f80d51590b467b5b
mahmad0610 <graphics.mahmad@gmail.com>
2025-07-18 23:38:25 +0500

    Update NoteRepository.java

--- src/main/java/com/example/echotype/NoteRepository.java
+++ src/main/java/com/example/echotype/NoteRepository.java
@@ -1,5 +1,8 @@
 package com.example.echotype;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface NoteRepository extends JpaRepository<Note, Long> {}
+public interface NoteRepository extends JpaRepository<Note, String> {
+    List<Note> findByUserId(Long userId);
+}
